32a9b6fb8a629b2b7723d03f7586fd8d87f4ccd0
rorybateman <70752417+rorybateman@users.noreply.github.com>
2024-04-11 17:38:24 +0100

    Update mpu6050_i2c.c

--- mpu6050/mpu6050_i2c.c
+++ mpu6050/mpu6050_i2c.c
@@ -102,7 +102,7 @@ int main() {
 
     while (1) {
         mpu6050_read_raw(acceleration, gyro, &temp);
-        printf("mk1");
+        printf("mk2");
         // These are the raw numbers from the chip, so will need tweaking to be really useful.
         // See the datasheet for more information
         printf("Acc. X = %d, Y = %d, Z = %d\n", acceleration[0], acceleration[1], acceleration[2]);
